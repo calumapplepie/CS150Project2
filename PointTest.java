ac496b47ffde26af3f28d390607dd0773ed5be61
Calum McConnell <calumlikesapplepie@gmail.com>
2021-11-11 00:18:21 -0500

    make point tests obscenely excessive

--- PointTest.java
+++ PointTest.java
@@ -7,7 +7,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.RepeatedTest;
 
 /**
- * The test class PointTest.
+ * This tests Point's for various properties and behaviors, using primarally handmade
+ * testcases
  *
  * @author  (your name)
  * @version (a version number or a date)
@@ -23,7 +24,9 @@ public class PointTest
      */
     public void testImmutableOptimization(){
         for(int i = -200; i < 200; i++){
-            immutableIdentityTester(i);
+            for(int j = -200; j < 200; j++){
+                immutableIdentityTester(i,j);
+            }
         }
     }
     
@@ -32,20 +35,21 @@ public class PointTest
      * It has been branched off to (hopefully) encourage the JVM to apply every
      * optimization it can, as well as to do some additional identity tests
      */
-    public void immutableIdentityTester(int i){
+    public void immutableIdentityTester(int i,int j){
         // lets see if java can optimize this at compile time
-        Point a = new Point(i,i);
-        Point b = new Point(i,i);
+        Point a = new Point(i,j);
+        Point b = new Point(i,j);
         // hashCode() is, by default, the system memory address.
         // we use assertNotEquals to pretend that this lack of optimization is good.
         assertNotEquals(a.hashCode(), b.hashCode());
+        
         // might as well check some other facts about identical points
-        // we'll need to test this further, elsewhere, but this is a good place to start
         assertEquals(a,b);
         assertTrue(a.equals(b));
         assertTrue(b.equals(a));
         // we should have 0 distance: I don't care that rounding exists, it shouldn't occur here!
         assertEquals(a.calculateDistance(b),0);
+        
         // one more thing: check that the distances work as they should with identity
         // that is: travelling 0 units creates a different point, and travelling to far
         // reproduces an existing point
